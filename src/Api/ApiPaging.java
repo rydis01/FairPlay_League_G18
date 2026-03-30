19bec10e8912d0ae8012291af501e31d0581aadf
tedted03 <theoandersson03@hotmail.com>
2026-03-30 14:53:10 +0200

    First api
    Skapade en grund och etablerade uppkoppling med dold nyckel

---
+++ src/Api/ApiPaging.java
@@ -0,0 +1,11 @@
+package Api;
+
+public class ApiPaging {
+    private int current;
+    private int total;
+
+    public int getCurrent() { return current; }
+    public void setCurrent(int current) { this.current = current; }
+    public int getTotal() { return total; }
+    public void setTotal(int total) { this.total = total; }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Api.ApiController;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ApiController controller = new ApiController();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hämtar data från API-Football...");
+
+        String result = controller.getCountriesData();
+
+        System.out.println("Resultat:");
+        System.out.println(result);
     }
 }
