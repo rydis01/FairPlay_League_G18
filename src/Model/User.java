e07a39a5b18a3a9a1238b64ab02549ca28ced67e
rydis01 <carl.rydengard@hotmail.com>
2026-03-30 14:21:10 +0200

    User start

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,47 @@
 package model;
 
 public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String epost;
+
+    public User(int id, String name, String password, String epost) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.epost = epost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
 }
