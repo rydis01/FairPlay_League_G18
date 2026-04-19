060ede3c80b0d2e2786bdb62bdd4bdac43a775ed
rydis01 <carl.rydengard@hotmail.com>
2026-04-19 15:35:20 +0200

    Poänghantering ska givetvis vara i ScoreService

--- src/service/CouponService.java
+++ src/service/CouponService.java
@@ -1,18 +1,15 @@
 package service;
 
 import database.CouponDAO;
-import database.MatchDAO;
 import model.Coupon;
 import java.util.Map;
 
 public class CouponService {
 
     private CouponDAO couponDAO;
-    private MatchDAO matchesDAO;
 
     public CouponService() {
         this.couponDAO = new CouponDAO();
-        this.matchesDAO = new MatchDAO();
     }
 
     // Skapa och spara en kupong med alla 8 tips
@@ -34,32 +31,6 @@ public class CouponService {
         Coupon coupon = new Coupon(userId, roundId);
         coupon.setTips(tips);
         couponDAO.saveCoupon(coupon);
-    }
-    // Carl
-    public int gradeCoupon(int userId, int roundId){
-        Coupon userCoupon = getCoupon(userId, roundId);
-
-        String[] correctCoupon = matchesDAO.getMatchresults(roundId);
-
-        // Jämför användarens tips med de rätta resultaten
-        int correctCount = 0;
-        Map<Integer, String> tips = userCoupon.getTips();
-
-        for (int i = 0; i < correctCoupon.length; i++) {
-            String userTip = tips.get(i);
-            String correctResult = correctCoupon[i];
-
-            // Om tippet matchar det rätta resultatet, öka räknaren
-            if (userTip != null && userTip.equals(correctResult)) {
-                correctCount++;
-            }
-        }
-
-        // Spara det rätta antalet på kupongen
-        userCoupon.setCorrectCount(correctCount);
-        couponDAO.updateCorrectCountCoupon(userCoupon);
-
-        return correctCount;
     }
 
     // Helpmethods
--- src/service/ScoreService.java
+++ src/service/ScoreService.java
@@ -1,9 +1,47 @@
 package service;
 
+import database.CouponDAO;
+import database.MatchDAO;
+import model.Coupon;
+import java.util.Map;
+
 /**
  * Hanterar poängberäkning och leaderboards.
  * Ansvarar för att räkna ut poäng efter varje omgång och visa topplistan.
  */
 public class ScoreService {
+    private CouponDAO couponDAO;
+    private MatchDAO matchesDAO;
 
+    public ScoreService() {
+        this.couponDAO = new CouponDAO();
+        this.matchesDAO = new MatchDAO();
+    }
+
+    // Rätta en kupong och returnera antalet rätt
+    public int gradeCoupon(int userId, int roundId) {
+        Coupon userCoupon = couponDAO.getCoupon(userId, roundId);
+
+        String[] correctCoupon = matchesDAO.getMatchresults(roundId);
+
+        // Jämför användarens tips med de rätta resultaten
+        int correctCount = 0;
+        Map<Integer, String> tips = userCoupon.getTips();
+
+        for (int i = 0; i < correctCoupon.length; i++) {
+            String userTip = tips.get(i);
+            String correctResult = correctCoupon[i];
+
+            // Om tippet matchar det rätta resultatet, öka räknaren
+            if (userTip != null && userTip.equals(correctResult)) {
+                correctCount++;
+            }
+        }
+
+        // Spara det rätta antalet på kupongen
+        userCoupon.setCorrectCount(correctCount);
+        couponDAO.updateCorrectCountCoupon(userCoupon);
+
+        return correctCount;
+    }
 }
